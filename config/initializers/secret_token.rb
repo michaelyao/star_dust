9d44d8b8da39d575355d0014a2ffc7d1bda5c792
myao <myao2005@gmail.com>
2018-05-01 22:41:57 -0700

    remove all old files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-StarDust::Application.config.secret_token = 'b5b27585a43bf7e8eb98548d588145d3a783a338fba102ddd78e01c5df0171589000e7e983e527ba77aada2c9dc31e1039cdca3b59696b1f4fa8a80d2c89a16d'
